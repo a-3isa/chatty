6276b15f59e0ce23abd27f87e45d5525ee0d47cc
a-3isa <a7med3isa000@gmail.com>
2025-09-04 22:18:50 +0300

    modified:   src/app.module.ts 	modified:   src/main.ts 	deleted:    src/messages/dto/create-message.dto.ts 	deleted:    src/messages/dto/update-message.dto.ts 	deleted:    src/messages/entities/message.entity.ts 	deleted:    src/messages/messages.controller.ts 	deleted:    src/messages/messages.module.ts 	deleted:    src/messages/messages.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
-import { MessagesModule } from './messages/messages.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -12,7 +11,6 @@ import { ConfigModule } from '@nestjs/config';
     UsersModule,
     AuthModule,
     ChatModule,
-    MessagesModule,
     ConfigModule.forRoot({
       isGlobal: true, // Makes env variables available everywhere
     }),
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,13 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips properties not in DTO
+      forbidNonWhitelisted: true, // throws error if extra properties are sent
+      transform: true, // auto-transforms payloads to DTO instances
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
